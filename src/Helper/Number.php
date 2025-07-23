a5c63453521b340837bcde6bb1c648d67e21ea98
knightowl1031 <35884482+knightowl1031@users.noreply.github.com>
2025-07-23 12:29:41 +0600

    init commit

---
+++ src/Helper/Number.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace KnightOwl1031\LaravelPrecisionCeil\Helpers;
+
+class Number
+{
+    /**
+     * Rounds a number up to a specified decimal precision.
+     *
+     * @param float $number The number to round.
+     * @param int $precision The number of decimal places to round to.
+     * @return float The rounded number.
+     */
+    public static function ceilPrecision(float $number, int $precision = 2): float
+    {
+        $multiplier = pow(10, $precision);
+        return ceil($number * $multiplier) / $multiplier;
+    }
+}
